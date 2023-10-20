ff463b854e427f926bfb128987e5b8bd4071f513
banksjt21 <113642612+banksjt21@users.noreply.github.com>
2023-10-20 16:45:01 -0400

    added react-query and security packages

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,9 +5,17 @@ import {
 	validateLoginInput,
 } from '../middleware/validationMiddleware.js';
 
+import rateLimiter from 'express-rate-limit';
+
+const apiLimiter = rateLimiter({
+	windowMs: 15 * 60 * 1000,
+	max: 15,
+	message: { msg: 'IP rate limit exceeded, retry in 15 minutes' },
+});
+
 import { login, logout, register } from '../controllers/authController.js';
-router.post('/register', validateRegisterInput, register);
-router.post('/login', validateLoginInput, login);
+router.post('/register', apiLimiter, validateRegisterInput, register);
+router.post('/login', apiLimiter, validateLoginInput, login);
 router.get('/logout', logout);
 
 export default router;
